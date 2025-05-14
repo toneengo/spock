a1469721f1951e5efa05b1c408ed8bfc6cc6c5a4
toneengo <qapt.tngo@gmail.com>
2025-05-15 08:26:43 +0930

    fix compile

--- include/spock/core.hpp
+++ include/spock/core.hpp
@@ -53,7 +53,7 @@ namespace spock {
     Buffer                create_buffer(size_t allocSize, VkBufferUsageFlags usage, VmaMemoryUsage memoryUsage);
 
     // ONLY use for gpu-only buffers at initialisation
-    Buffer copy_to_buffer(VkBuffer buffer, void* src, VkDeviceSize srcOffset, VkDeviceSize dstOffset, size_t size);
+    Buffer copy_to_buffer(VkBuffer buffer, void* src, VkDeviceSize srcOffset, VkDeviceSize dstOffset, VkDeviceSize size);
     void                  destroy_buffer(Buffer buffer);
 
     void                  create_swapchain(uint32_t width, uint32_t height);
