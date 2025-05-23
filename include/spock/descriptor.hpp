fb6bb56217ec24cb6d9f07657ceae60c60eca7ab
toneengo <qapt.tngo@gmail.com>
2025-05-23 17:19:18 +0930

    revert to multi type desc allocators

--- include/spock/descriptor.hpp
+++ include/spock/descriptor.hpp
@@ -4,26 +4,9 @@
 
 namespace spock {
     struct DescriptorAllocator {
-        VkDescriptorType type;
-        uint32_t         startSize;
-
-        bool                          initialised = false;
-        std::vector<VkDescriptorPool> pools;
-        uint32_t                      currentPool = 0;
-        uint32_t                      setsPerPool = 0;
-        VkDescriptorPoolCreateFlags   flags       = 0;
-
-        void                          init(VkDescriptorType type, uint32_t startSize);
-        VkDescriptorPool              create_pool();
-        void                          set_flags(VkDescriptorPoolCreateFlags flags);
-        void                          clear_pools();
-        void                          destroy_pools();
-        VkDescriptorSet               allocate(VkDescriptorSetLayout layout);
-    };
-    struct GrowableDescriptorAllocator {
         struct PoolSizeRatio {
             VkDescriptorType type;
-            float            ratio;
+            uint32_t         ratio;
         };
 
         bool                          initialised = false;
